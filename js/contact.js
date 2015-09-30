708f3d24a93621c80f0a2988464a19a2ea7dd523
mkirwa <mahlonkibiwott@gmail.com>
2015-09-30 15:22:04 -0400

    form

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 	$('#contact-form').submit(function() {
+		if($('#contact-form').hasClass('clicked')){
+				return false;
+			}
+		
+		$('#contact-form').addClass('clicked');
+		
 		var buttonWidth=$('#contact-form button').width();
 		
 		var buttonCopy = $('#contact-form button').html(),
@@ -14,14 +20,14 @@ $(document).ready(function() {
 		$('.requiredField').each(function() {
 			if($.trim($(this).val()) == '') {
 				var errorText = $(this).data('error-empty');
-				$(this).parents('.field-wrap').append('<span class="error-message" style="display:none;">'+errorText+'.</span>').find('.error-message').fadeIn('fast');
+				$(this).parent().append('<span class="error-message">'+errorText+'.</span>');
 				$(this).addClass('inputError');
 				hasError = true;
 			} else if($(this).is("input[type='email']") || $(this).attr('name')==='email') {
 				var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 				if(!emailReg.test($.trim($(this).val()))) {
 					var invalidEmail = $(this).data('error-invalid');
-					$(this).parents('.field-wrap').append('<span class="error-message" style="display:none;">'+invalidEmail+'.</span>').find('.error-message').fadeIn('fast');
+					$(this).parent().append('<span class="error-message">'+invalidEmail+'.</span>');
 					$(this).addClass('inputError');
 					hasError = true;
 				}
@@ -29,27 +35,41 @@ $(document).ready(function() {
 		});
 		
 		if(hasError) {
-			$('#contact-form button').html('<i class="fa fa-times"></i>'+errorMessage);
+			$('#contact-form button').html('<i class="fa fa-remove"></i>'+errorMessage);
 			setTimeout(function(){
 				$('#contact-form button').html(buttonCopy);
 				$('#contact-form button').width('auto');
+				$('#contact-form').removeClass('clicked');
 			},2000);
 		}
 		else {
-			$('#contact-form button').html('<i class="fa fa-spinner fa-spin"></i>'+sendingMessage);
+			$('#contact-form button').html('<i class="fa fa-refresh fa-spin"></i>'+sendingMessage);
 			
 			var formInput = $(this).serialize();
+
 			$.post($(this).attr('action'),formInput, function(data){
 				$('#contact-form button').html('<i class="fa fa-check"></i>'+okMessage);
-				
 				$('#contact-form')[0].reset();
-				
+
 				setTimeout(function(){
-					$('#contact-form button').html(buttonCopy);
-					$('#contact-form button').width('auto');
-				},2000);
-				
+					
+				$('#contact-form button').html(buttonCopy);
+				$('#contact-form button').width('auto');
+				$('#contact-form').removeClass('clicked');
+			},2000);
 			});
+
+
+			setTimeout(function(){
+				setTimeout(function(){
+				$('#contact-form button').html(buttonCopy);
+				$('#contact-form button').width('auto');
+				$('#contact-form').removeClass('clicked');
+			},1000);
+				$('#contact-form button').html('<i class="fa fa-check"></i>'+okMessage);
+				$('#contact-form')[0].reset();
+			},2000);
+			
 		}
 		
 		return false;	
